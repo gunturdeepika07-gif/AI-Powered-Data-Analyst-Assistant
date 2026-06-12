472b0fb42c738fc4ed365839406488b10c3dcaad
Deepika Guntur <gunturdeepika07@gmail.com>
2026-06-12 12:36:47 +0530

    Fixed sidebar navigation and page-wise functionality

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ from src.ai_assistant import ask_gemini
 from src.report_generator import generate_report
 from src.recommendations import generate_recommendations
 
-
 # Import CSV loader
 from src.data_loader import load_data
 
@@ -42,13 +41,20 @@ uploaded_file = st.file_uploader(
     type = ["csv"]
 )
 
-
 #Run analysis only after a file is uploaded
 if uploaded_file:
 
 
     #Load CSV into DataFrame
     df = load_data(uploaded_file)
+
+    st.subheader("Data Cleaning")
+
+    if st.checkbox("Remove Duplicate Rows"):
+        duplicates = df.duplicated().sum()
+        df = df.drop_duplicates()
+
+        st.success(f"Removed {duplicates} duplicate rows")
 
     with st.expander(
         "Dataset Preview"
@@ -69,178 +75,211 @@ if uploaded_file:
     st.write(f"Rows: {rows}")
     st.write(f"Columns: {cols}")
 
-
     st.sidebar.title("AI Data Analyst Assistant")
 
-    page = st.sidebar.radio("Navigate",["Overview", "Visualization", "AI Features"])
+    page = st.sidebar.radio(
+        "Navigate",
+        ["Overview", "Visualization", "AI Insights"]
+    )
     st.sidebar.write(f"Rows: {rows}")
     st.sidebar.write(f"Columns: {cols}")
 
-    # Show descriptive statistics
-    st.subheader("Dataset Statistics")
-    st.write(df.describe())
+    if page == "Overview":
+        st.subheader("Dataset Cleaning")
 
-    # Show column Data types
-    st.subheader("Data Types")
-    st.write(get_dtypes(df))
+        st.write(df.head())
+        st.write(df.describe())
 
-    # Show missing values count
-    st.subheader("Missing values")
-    missing_values = get_misssing_values(df)
-    st.write(missing_values)
+            # Show descriptive statistics
+        st.subheader("Dataset Statistics")
+        st.write(df.describe())
 
-    total_missing = missing_values.sum()
-    st.write(f"Total Missing Values: {total_missing}")
+        # Show column Data types
+        st.subheader("Data Types")
+        st.write(get_dtypes(df))
+
+        # Show missing values count
+        st.subheader("Missing values")
+        missing_values = get_misssing_values(df)
+        st.write(missing_values)
+
+        total_missing = missing_values.sum()
+        st.write(f"Total Missing Values: {total_missing}")
 
 
-    st.sidebar.title("AI Data Analyst Assistant")
+        # Data Cleaning by removing missing values
+        st.header("Data Cleaning")
 
-    page = st.sidebar.radio("Navigate",["Overview", "Visualization", "AI Features"])
-    st.sidebar.write(f"Rows: {rows}")
-    st.sidebar.write(f"Columns: {cols}")
+        if st.button("Remove Missing Values"):
+            df = df.dropna()
 
-
-    # Data Cleaning by removing missing values
-    st.header("Data Cleaning")
-
-    if st.button("Remove Missing Values"):
-        df = df.dropna()
-
-        st.success("Missing values removed successfully!")
+            st.success("Missing values removed successfully!")
         st.write(df.head())
 
         rows, cols = df.shape
 
         st.write(f"Updated Shape: {rows} rows x {cols} columns")
 
-    if st.button("Remove Duplicates"):
-        df = df.drop_duplicates()
+        if st.checkbox("Drop Missing Values"):
+            df = df.dropna()
 
-        st.success("Duplicate rows removed!")
-        st.write(df.head())
+            st.success("Missing values removed.")
 
+        if st.checkbox("Fill Numeric Missing Values with Mean"):
+            numeric_col = df.select_dtypes(include="number").columns
 
-    # Find all numeric columns automatically
-    numeric_columns = df.select_dtypes(
-        include = ['int64', 'float64']
-    ).columns
+            for col in numeric_col:
+                df[col] = df[col].fillna(
+                    df[col].mean()
+                )
+            st.success("Numeric missing values filled.")
 
-   
-    # Column Explorer section
-    st.subheader("Column Explorer")
+        if st.button("Remove Duplicates"):
+            df = df.drop_duplicates()
 
-    numeric_columns = df.select_dtypes(include="number").columns
-
-    # Display available numeric columns
-    st.write("Numeric Columns Found: ")
-    st.write(list(numeric_columns))
-
-    selected_column = st.selectbox(
-        "Select Numeric Column",
-        numeric_columns
-    )
-
-    st.subheader("AI Insights")
-
-    insights = generate_insights(df, selected_column)
-
-    for insight in insights:
-        st.write(insight)
-
-        st.subheader("AI Generated Summary")
-
-        summary = generate_summary(df, selected_column)
-
-        st.write(summary)
+            st.success("Duplicate rows removed!")
+            st.write(df.head())
 
 
-    # Histogram visualization
+        # Find all numeric columns automatically
+        numeric_columns = df.select_dtypes(
+            include = ['int64', 'float64']
+        ).columns
+    
+        # Column Explorer section
+        st.subheader("Column Explorer")
 
-    numeric_columns=df.select_dtypes(
-        include=['int64', 'float64']
-    ).columns
+        numeric_columns = df.select_dtypes(include="number").columns
 
-    if len(numeric_columns) == 0:
-        st.warning("No numeric columns found for analysis.")
-        st.stop()
-    st.subheader("Histogram")
-    fig = create_histogram(
-        df,
-        selected_column
-    )
-    st.pyplot(fig)
+        # Display available numeric columns
+        st.write("Numeric Columns Found: ")
+        st.write(list(numeric_columns))
 
-
-    # Boxplot visualization
-    st.subheader("Boxplot")
-    box_fig = create_boxplot(
-        df,
-        selected_column
-    )
-    st.pyplot(box_fig)
-
-    # Correlation analysis
-    st.subheader("Correlation Heatmap")
-    heatmap_fig = create_heatmap(df)
-    st.pyplot(heatmap_fig)
-
-
-    st.subheader("Ask Questions About Your Dataset")
-
-    question = st.text_input(
-        'Ask a question'
-    )
-
-    if question:
-        response = answer_question(df, question)
-
-        st.success(response)
-
-    # AI Chat
-
-    st.subheader("AI Data Analyst Chat")
-
-    ai_question = st.text_area(
-        "Ask anything about your dataset"
-    )
-
-    if st.button("Generate AI Response"):
-        if not ai_question.strip():
-
-            st.warning(
-            "Please enter a question."
+        selected_column = st.selectbox(
+            "Select Numeric Column",
+            numeric_columns
         )
 
-    else:
+    elif page == "Visualization":
+        st.subheader("Histogram")
+        # Histogram visualization
 
-        with st.spinner("Analyzing dataset..."):
+        numeric_columns=df.select_dtypes(
+            include=['int64', 'float64']
+        ).columns
 
-            response = ask_gemini(
-                df,
-                ai_question
+        if len(numeric_columns) == 0:
+            st.warning("No numeric columns found for analysis.")
+            st.stop()
+        st.subheader("Histogram")
+        st.subheader("Column Explorer")
+
+        numeric_columns = df.select_dtypes(include="number").columns
+
+        selected_column = st.selectbox(
+            "Select Numeric Column",
+            numeric_columns
+        )
+        fig = create_histogram(
+            df,
+            selected_column
+        )
+        st.pyplot(fig)
+
+        # Boxplot visualization
+        st.subheader("Boxplot")
+        box_fig = create_boxplot(
+            df,
+            selected_column
+        )
+        st.pyplot(box_fig)
+
+        # Correlation analysis
+        st.subheader("Correlation Heatmap")
+        heatmap_fig = create_heatmap(df)
+        st.pyplot(heatmap_fig)
+        
+
+
+    elif page == "AI Insights":
+        st.subheader("AI Insights")
+        st.subheader("Column Explorer")
+
+        numeric_columns = df.select_dtypes(include="number").columns
+
+        selected_column = st.selectbox(
+            "Select Numeric Column",
+            numeric_columns
+        )
+
+        insights = generate_insights(df, selected_column)
+
+        for insight in insights:
+            st.write(insight)
+
+            st.subheader("AI Generated Summary")
+
+            summary = generate_summary(df, selected_column)
+
+            st.write(summary)
+
+
+        st.subheader("Ask Questions About Your Dataset")
+
+        question = st.text_input(
+            'Ask a question'
+        )
+
+        if question:
+            response = answer_question(df, question)
+
+            st.success(response)
+
+        # AI Chat
+
+        st.subheader("AI Data Analyst Chat")
+
+        ai_question = st.text_area(
+            "Ask anything about your dataset"
+        )
+
+        if st.button("Generate AI Response"):
+            if not ai_question.strip():
+
+                st.warning(
+                "Please enter a question."
             )
 
-        st.success(response)
+        else:
 
-        report = generate_report(
-            selected_column,
-            insights,
-            summary,
-            response
-        )
+            with st.spinner("Analyzing dataset..."):
 
-        st.download_button(
-            label="Download Analysis Report",
-            data=report,
-            file_name="analysis_report.txt",
-            mime="text/plain"
-        )
-        st.subheader("AI Recommendations")
+                response = ask_gemini(
+                    df,
+                    ai_question
+                )
 
-        recommendations = generate_recommendations(df)
+            st.success(response)
 
-        for recommendation in recommendations:
-            st.info(recommendation)
+            report = generate_report(
+                selected_column,
+                insights,
+                summary,
+                response
+            )
 
-        st.markdown("---")
-        st.caption("Build using Python, Streamlit, Pandas, and Gemini API")
+            st.download_button(
+                label="Download Analysis Report",
+                data=report,
+                file_name="analysis_report.txt",
+                mime="text/plain"
+            )
+            st.subheader("AI Recommendations")
+
+            recommendations = generate_recommendations(df)
+
+            for recommendation in recommendations:
+                st.info(recommendation)
+
+    st.markdown("---")
+    st.caption("Build using Python, Streamlit, Pandas, and Gemini API")
